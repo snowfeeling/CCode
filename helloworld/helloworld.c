0b62ac008f5a0698e508ccae9a0ea202179e61e9
snowfeeling <snow_feeling@hotmail.com>
2022-10-03 00:44:51 +0800

    new file:   helloworld/.vscode/launch.json 	new file:   helloworld/.vscode/tasks.json 	new file:   helloworld/helloworld.c

---
+++ helloworld/helloworld.c
@@ -0,0 +1,12 @@
+//=============================
+// The Hello World program.
+// Created by Wangss 2022-10-02
+
+#include <stdio.h>
+
+int main()
+{
+   printf("Helllo World.");
+   printf("\nI am listening to the rain.");
+   return 0; 
+}
