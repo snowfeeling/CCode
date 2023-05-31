b1663021471972894eafe16ff518a0edd0913e8b
snowfeeling <snow_feeling@hotmail.com>
2023-05-31 17:19:13 +0800

    Separate the snake program.

---
+++ snake/app/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <time.h>
+
+
+#include "../inc/mylog.h"
+#include "../inc/snake.h"
+
+int main ()
+{
+
+    //test_bplus_tree();
+    snake();
+    return 0;
+
+}
