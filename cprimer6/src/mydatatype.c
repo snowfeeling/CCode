362b23c2c82533977090b3845705c72b5d13fc7a
snowfeeling <snow_feeling@hotmail.com>
2025-06-15 01:56:14 +0800

    SF

--- cprimer6/src/mydatatype.c
+++ cprimer6/src/mydatatype.c
@@ -842,5 +842,6 @@ git config --global --unset http.proxy
 git config --global --unset https.proxy
 git config --global http.proxy http://127.0.0.1:7890
 git config --global https.proxy http://127.0.0.1:7890
-
+git config --global http.proxy 127.0.0.1:29290
+git config --global https.proxy 127.0.0.1:29290
 */
